5de3886c7eb1c409fa7e5132f5466bb801a5bfc2
heungryuncho <heungryuncho@naver.com>
2022-09-01 22:10:43 +0900

    first commit

---
+++ src/test/java/com/example/springsecuritypractice1/SpringSecurityPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritypractice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPractice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
